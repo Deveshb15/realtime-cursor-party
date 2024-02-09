7245150fdbda2e7633c9951e390dce6fee0d7668
Deveshb15 <deveshb583@gmail.com>
2024-02-09 21:13:19 +0530

    using already deployed partykit server

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,9 @@ import Image from "next/image";
 import CursorProvider from "./context/cursor-context";
 import Space from "./components/space";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
-  // console.log("SEARCH PARAMS ", searchParams);
-  const room =
-    typeof searchParams?.partyroom === "string"
-      ? searchParams.partyroom
-      : "devesh-room";
-
-  const host =
-    typeof searchParams?.partyhost === "string"
-      ? searchParams.partyhost
-      : "voronoi-party.genmon.partykit.dev";
-
+export default function Home() {
   return (
-    <CursorProvider host={host} room={room}>
+    <CursorProvider host="voronoi-party.genmon.partykit.dev" room="devesh-room">
       <Space />
     </CursorProvider>
   );
